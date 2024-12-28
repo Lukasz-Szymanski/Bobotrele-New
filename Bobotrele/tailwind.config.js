4950b0e15de2886c81a4b1675ca6ae74b0396ba3
Łukasz Szymański <lukasz.szymanski94@icloud.com>
2024-12-28 13:13:18 +0100

    Create new prroject

---
+++ Bobotrele/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"], // zmiana ścieżki, aby uwzględnić pliki w głównym katalogu
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
